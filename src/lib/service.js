454356037fb6c6d96970505dcfec66acacfb87ba
songulkarahan <songul.karahan@bil.omu.edu.tr>
2020-12-23 04:09:17 +0300

    getData() function defined

---
+++ src/lib/service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const ENDPOINT = "https://jsonplaceholder.typicode.com";
+
+async function getData(userId) {
+  try {
+    const { data: users } = await axios.get(`${ENDPOINT}/users/${userId}`);
+    const { data: posts } = await axios.get(
+      `${ENDPOINT}/posts/?userId=${userId}`
+    );
+    users.posts = posts;
+    console.log(users);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//default export et
